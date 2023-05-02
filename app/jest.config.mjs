fef21e6f9c7dbf1a15709fea3a422ab759e729dd
Ian Mancini <ian.mancini@mercadolibre.com>
2023-05-02 04:42:58 +0200

    chore: add and setup jest + react's testing library

---
+++ app/jest.config.mjs
@@ -0,0 +1,16 @@
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+};
+
+export default createJestConfig(config);
